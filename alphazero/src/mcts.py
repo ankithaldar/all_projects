1c9a307ec1ef472ff44433f8560c1275d13a4f9c
Ankit Haldar <ankithaldar@gmail.com>
2024-06-21 13:05:13 +0530

    Adding ResNet Model to the game

--- alphazero/src/mcts.py
+++ alphazero/src/mcts.py
@@ -7,6 +7,7 @@
 # imports
 import numpy as np
 import math
+import tensorflow as tf
 #    script imports
 # imports
 
@@ -19,22 +20,22 @@ import math
 class Node:
   '''Node for MCTS'''
 
-  def __init__(self, game, args, state, parent=None, action_taken=None):
+  def __init__(self, game, args, state, parent=None, action_taken=None, prior=0):
     self.game = game
     self.args = args
     self.state = state
     self.parent = parent
     self.action_taken = action_taken
+    self.prior = prior
 
     self.children = []
-    self.expandable_moves = game.get_valid_moves(state)
 
     self.visit_count = 0
     self.value_sum = 0
 
 
   def is_fully_expanded(self):
-    return np.sum(self.expandable_moves) == 0 and len(self.children) > 0
+    return len(self.children) > 0
 
 
   def select(self):
@@ -51,44 +52,23 @@ class Node:
 
 
   def get_ucb(self, child):
-    q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
-    return q_value + self.args['C'] * math.sqrt(math.log(self.visit_count) / child.visit_count)
+    if child.visit_count == 0:
+      q_value = 0
+    else:
+      q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
+
+    return q_value + self.args['C'] * (math.sqrt(self.visit_count) / (child.visit_count + 1)) * child.prior
 
 
-  def expand(self):
-    action = np.random.choice(np.where(self.expandable_moves == 1)[0])
-    self.expandable_moves[action] = 0
+  def expand(self, policy):
+    for action, prob in enumerate(policy):
+      if prob > 0:
+        child_state = self.state.copy()
+        child_state = self.game.get_next_state(child_state, action, 1)
+        child_state = self.game.change_perspective(child_state, player=-1)
 
-    child_state = self.state.copy()
-    child_state = self.game.get_next_state(child_state, action, 1)
-    child_state = self.game.change_perspective(child_state, player=-1)
-
-    child = Node(self.game, self.args, child_state, self, action)
-    self.children.append(child)
-    return child
-
-
-  def simulate(self):
-    value, is_terminal = self.game.get_value_and_terminated(self.state, self.action_taken)
-    value = self.game.get_opponent_value(value)
-
-    if is_terminal:
-      return value
-
-    rollout_state = self.state.copy()
-    rollout_player = 1
-    while True:
-      valid_moves = self.game.get_valid_moves(rollout_state)
-      action = np.random.choice(np.where(valid_moves == 1)[0])
-      rollout_state = self.game.get_next_state(rollout_state, action, rollout_player)
-      value, is_terminal = self.game.get_value_and_terminated(rollout_state, action)
-      if is_terminal:
-        if rollout_player == -1:
-          value = self.game.get_opponent_value(value)
-        return value
-
-      rollout_player = self.game.get_opponent(rollout_player)
-
+        child = Node(self.game, self.args, child_state, self, action, prob)
+        self.children.append(child)
 
 
   def backpropagate(self, value):
@@ -103,9 +83,10 @@ class Node:
 class MCTS:
   '''Monte Carlo Tree Search'''
 
-  def __init__(self, game, args):
+  def __init__(self, game, args, model):
     self.game = game
     self.args = args
+    self.model = model
 
 
   def search(self, state):
@@ -121,8 +102,16 @@ class MCTS:
       value = self.game.get_opponent_value(value)
 
       if not is_terminal:
-        node = node.expand()
-        value = node.simulate()
+        policy, value = self.model(
+          tf.expand_dims(input=self.game.get_encoded_state(node.state), axis=0)
+        )
+
+        policy = tf.squeeze(tf.nn.softmax(policy), axis=None).numpy()
+        valid_moves = self.game.get_valid_moves(node.state)
+        policy *= valid_moves
+        policy /= np.sum(policy)
+        value = tf.squeeze(value).numpy()
+        node.expand(policy)
 
       node.backpropagate(value)
 
