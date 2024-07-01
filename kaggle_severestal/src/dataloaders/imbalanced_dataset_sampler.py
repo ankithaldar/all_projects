6dc5973da35ddcdbb5ac0c2a0bc0f271e338b629
Ankit Haldar <ankithaldar@gmail.com>
2024-07-01 14:32:51 +0530

    Move Kaggle - Severestal from old git repository into this

---
+++ kaggle_severestal/src/dataloaders/imbalanced_dataset_sampler.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author: Ankit Haldar
+
+'''Doc String for the module'''
+
+
+# imports
+#    script imports
+# imports
+
+
+# constants
+# constants
+
+# https://github.com/ufoym/imbalanced-dataset-sampler/blob/master/torchsampler/imbalanced.py
+
+# classes
+class ClassName:
+  '''docstring for ClassName'''
+
+  def __init__(self, arg):
+    super().__init__()
+    self.arg = arg
+
+
+# classes
+
+
+# functions
+def function_name():
+  pass
+# functions
+
+
+# main
+def main():
+  pass
+
+
+# if main script
+if __name__ == '__main__':
+  main()
