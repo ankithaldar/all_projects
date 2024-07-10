8b15e7759529238628123ed16d3f79422b6910e3
Ankit Haldar <ankithaldar@gmail.com>
2024-07-11 00:12:13 +0530

    Implement the Logic for connect 4

--- alphazero/main.py
+++ alphazero/main.py
@@ -7,6 +7,7 @@
 # imports
 #    script imports
 from src.tictactoe import TicTacToe
+from src.connect_four import ConnectFour
 from src.mcts import MCTS
 from src.model import ResNet
 from src.alphazero import AlphaZero
@@ -42,7 +43,7 @@ def run_tests():
   )
 
 
-def test_self_play():
+def test_tictactoe_self_play():
   tictactoe = TicTacToe()
 
   model = ResNet(tictactoe, 4, 64)
@@ -58,13 +59,36 @@ def test_self_play():
 
   alpha_zero = AlphaZero(model, tictactoe, args)
   alpha_zero.learn()
+
+
+
+def test_connectfour_self_play():
+  connect_four = ConnectFour()
+
+  model = ResNet(connect_four, 9, 128)
+
+  args = {
+    'C': 2,
+    'num_searches': 60,
+    'num_iterations': 3,
+    'num_selfPlay_iterations': 500,
+    'num_epochs': 4,
+    'batch_size': 64,
+    'temperature': 1.25,
+    'dirichlet_epsilon': 0.25,
+    'dirichlet_alpha': 0.3
+  }
+
+  alpha_zero = AlphaZero(model, connect_four, args)
+  alpha_zero.learn()
 # functions
 
 
 # main
 def main():
   # run_tests()
-  test_self_play()
+  test_tictactoe_self_play()
+  test_connectfour_self_play()
 
 
 # if main script
