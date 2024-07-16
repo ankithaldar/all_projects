f1a206a74f7799509bc9c0fa547f1aa2faf008a4
Ankit Haldar <ankithaldar@gmail.com>
2024-07-16 21:37:50 +0530

    Documentation

--- cat_game_rl/src/environment/agent.py
+++ cat_game_rl/src/environment/agent.py
@@ -12,7 +12,16 @@ from abc import ABC
 
 
 class Agent(ABC):
-  '''Base class for Agent in RL'''
+  '''Abstract base class for an agent in Reinforcement Learning (RL).
+
+    This class defines the core interface for an RL agent, requiring all
+    subclasses to implement the act method.
+
+    Args:
+      control: An object representing the control input to the agent. The
+      specific format of the control object will depend on the
+      environment the agent interacts with.
+  '''
 
   def act(self, control):
     pass
