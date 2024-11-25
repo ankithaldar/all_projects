971e9652935573ef3800b7657c88178fdc74caca
Ankit Haldar <ankithaldar@gmail.com>
2024-11-25 20:12:00 +0530

    00003.model training engine init

---
+++ kaggle_severstal/src/train.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Train the model base script'''
+
+from argparse import ArgumentParser
+from engines.classification_engine import ClassificationEngine
+from config_params.params_reader import Parameters
+
+
+def main(module_params=None):
+  '''main function'''
+  pe = ClassificationEngine(module_params)
+  pe.train(module_params.run_params)
+
+
+if __name__ == '__main__':
+
+  parser = ArgumentParser(parents=[])
+  parser.add_argument('--params_yml', type=str)
+  params_file_path = parser.parse_args()
+
+  hparams = Parameters(params_file_path.params_yml)
+
+  main(module_params=hparams)
