6f9aebe094efa10cf546b6ad0f996060ee48aef7
Ankit Haldar <ankithaldar@gmail.com>
2025-02-12 18:45:51 +0530

    Initial RL implementation

---
+++ cat_game_rl/src/rl_cat_game_env.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Doc String for the module'''
+
+
+# imports
+#    script imports
+from env_game_world import worldbuilder_create
+# imports
+
+
+# constants
+# constants
+
+
+# classes
+class CatGameEnv:
+  '''RL environment for Cat Game'''
+
+  def __init__(self):
+    pass
+
+  def reset(self):
+    self.world = worldbuilder_create()
+
+# classes
+
+
+# functions
+def function_name():
+  pass
+# functions
+
+
+# main
+def main():
+  pass
+
+
+# if main script
+if __name__ == '__main__':
+  main()
